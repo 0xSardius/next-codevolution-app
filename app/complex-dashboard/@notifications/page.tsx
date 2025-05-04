536405c1f1d0b833ffad20603727e27438ba22ce
Sardius <0xsardius@gmail.com>
2025-05-04 16:34:41 -0400

    parallel routes

---
+++ app/complex-dashboard/@notifications/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/app/components/Card";
+
+export default function Notifications() {
+  return (
+    <Card>
+      <h1>Notifications</h1>
+    </Card>
+  );
+}
---
+++ app/complex-dashboard/layout.tsx
@@ -0,0 +1,20 @@
+export default function ComplexDashboardLayout({
+  children,
+  users,
+  revenue,
+  notifications,
+}: {
+  children: React.ReactNode;
+  users: React.ReactNode;
+  revenue: React.ReactNode;
+  notifications: React.ReactNode;
+}) {
+  return (
+    <>
+      <div>{children}</div>
+      <div>{users}</div>
+      <div>{revenue}</div>
+      <div>{notifications}</div>
+    </>
+  );
+}
